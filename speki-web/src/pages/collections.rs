501ace7fea1652437581274cb3287777a154694b
tor <torberge@outlook.com>
2025-01-09 01:16:13 +0100

    add new collections

--- speki-web/src/pages/collections.rs
+++ speki-web/src/pages/collections.rs
@@ -1,17 +1,35 @@
+use std::sync::Arc;
+
 use dioxus::prelude::*;
 use speki_core::collection::Collection;
 
-use crate::APP;
+use crate::{overlays::textinput::TextInput, APP, OVERLAY};
 
 #[component]
 pub fn Collections() -> Element {
     let collections: Signal<Vec<Collection>> = use_signal(|| vec![]);
     spawn(async move {
         let cols = APP.read().load_collections().await;
-        collections.clone().set(cols);
+        if cols.len() != collections.read().len() {
+            collections.clone().set(cols);
+        }
     });
 
     rsx! {
+        button {
+            onclick: move |_| {
+                let f = move |name: String| {
+                    let col = Collection::new(name);
+                    spawn(async move {
+                        APP.read().save_collection(col).await;
+                    });
+                };
+
+                let txt = TextInput::new("add collection".to_string(), Arc::new(Box::new(f)));
+                OVERLAY.read().set(Box::new(txt));
+            },
+            "add collection"
+        }
         for col in collections() {
             p {"{col.name}"}
         }
