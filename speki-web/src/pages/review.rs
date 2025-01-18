47b821ad5c476c79d627bcb5e3677fb578eb8685
tor <torberge@outlook.com>
2025-01-18 16:50:45 +0100

    add progress indicator on collections

--- speki-web/src/pages/review.rs
+++ speki-web/src/pages/review.rs
@@ -12,7 +12,7 @@ use crate::{
 #[derive(Clone)]
 pub struct ReviewPage {
     filter: FilterEditor,
-    collections: Signal<Vec<Collection>>,
+    collections: Signal<Vec<(Collection, RecallDist)>>,
 }
 
 impl ReviewPage {
@@ -26,7 +26,13 @@ impl ReviewPage {
 
         spawn(async move {
             let _cols = APP.read().load_collections().await;
-            cols.clone().set(_cols);
+            let mut out = vec![];
+
+            for col in _cols {
+                let dist = RecallDist::new(col.clone()).await;
+                out.push((col, dist));
+            }
+            cols.clone().set(out);
         });
 
         selv
@@ -61,22 +67,113 @@ pub fn Review() -> Element {
     }
 }
 
+#[component]
+fn RecallBar(dist: RecallDist) -> Element {
+    let proportions = dist.proportions();
+
+    rsx!(
+        div {
+            class: "flex w-full h-4 rounded overflow-hidden border border-gray-300",
+            for (percentage, color) in proportions {
+                div {
+                    style: format!(
+                        "width: {}%; background-color: {};",
+                        percentage,
+                        color
+                    ),
+                    key: "{color}",
+                }
+            }
+        }
+    )
+}
+
+#[derive(Default, Clone, PartialEq, Debug)]
+struct RecallDist {
+    p: u32,
+    n1: u32,
+    n2: u32,
+    n3: u32,
+    n4: u32,
+    n5: u32,
+    n6: u32,
+}
+
+impl RecallDist {
+    const HEXP: &str = "#00FFFF";
+    const HEX1: &str = "#FF0D0D";
+    const HEX2: &str = "#FF4E11";
+    const HEX3: &str = "#FF8E15";
+    const HEX4: &str = "#FAB733";
+    const HEX5: &str = "#ACB334";
+    const HEX6: &str = "#69B34C";
+
+    fn total(&self) -> u32 {
+        self.p + self.n1 + self.n2 + self.n3 + self.n4 + self.n5 + self.n6
+    }
+
+    fn proportions(&self) -> Vec<(f32, &'static str)> {
+        let total = self.total();
+        if total == 0 {
+            return vec![];
+        }
+        vec![
+            (self.p as f32 / total as f32 * 100.0, Self::HEXP),
+            (self.n1 as f32 / total as f32 * 100.0, Self::HEX1),
+            (self.n2 as f32 / total as f32 * 100.0, Self::HEX2),
+            (self.n3 as f32 / total as f32 * 100.0, Self::HEX3),
+            (self.n4 as f32 / total as f32 * 100.0, Self::HEX4),
+            (self.n5 as f32 / total as f32 * 100.0, Self::HEX5),
+            (self.n6 as f32 / total as f32 * 100.0, Self::HEX6),
+        ]
+    }
+
+    async fn new(col: Collection) -> Self {
+        let mut selv = Self::default();
+
+        for card in col.expand(APP.read().inner().card_provider()).await {
+            *match card.recall_rate() {
+                Some(rate) => {
+                    if rate < 0.05 {
+                        &mut selv.n1
+                    } else if rate < 0.2 {
+                        &mut selv.n2
+                    } else if rate < 0.5 {
+                        &mut selv.n3
+                    } else if rate < 0.8 {
+                        &mut selv.n4
+                    } else if rate < 0.95 {
+                        &mut selv.n5
+                    } else {
+                        &mut selv.n6
+                    }
+                }
+                None => &mut selv.p,
+            } += 1;
+        }
+
+        tracing::info!("{selv:?}");
+
+        selv
+    }
+}
+
 fn render_collections(state: ReviewPage) -> Element {
     let filter = state.filter.to_filter();
     let collections = state.collections.clone();
 
-    let mut colfil: Vec<(Collection, CardFilter)> = vec![];
+    let mut colfil: Vec<(Collection, RecallDist, CardFilter)> = vec![];
 
-    for col in collections.cloned() {
-        colfil.push((col, filter.clone()));
+    for (col, dist) in collections.cloned() {
+        colfil.push((col, dist, filter.clone()));
     }
 
     rsx! {
         div {
-            class: "flex flex-col max-w-[350px] mr-5",
+            class: "flex flex-col max-w-[550px] mr-5",
 
             button {
-                class: "inline-flex items-center text-white bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mb-2",
+                class: "inline-flex items-center text-white bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mb-8",
                 onclick: move |_| {
                     let filter = filter.clone();
                     spawn(async move {
@@ -89,43 +186,48 @@ fn render_collections(state: ReviewPage) -> Element {
 
             }
 
-            for (col, filter) in colfil {
+            for (col, dist, filter) in colfil {
                 div {
+                    class: "flex flex-col mb-8",
+                    div {
                     class: "flex flex-row",
-                    button {
-                        class: "inline-flex items-center text-white bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mb-2",
-                        onclick: move |_| {
-                            let filter = filter.clone();
-                            spawn(async move {
-                                let col = APP.read().load_collection(col.id).await;
-                                let cards = col.expand(APP.read().inner().card_provider.clone()).await;
-                                let session = ReviewState::new_with_filter(cards, filter).await;
-                                OVERLAY.cloned().set(Box::new(session));
-                            });
-                        },
-                        "{col.name}"
+                        button {
+                            onclick: move |_|{
+                                spawn(async move {
+                                    let viewer = ColViewer::new(col.id).await;
+                                    OVERLAY.read().set(Box::new(viewer));
+                                });
+                            },
+                            "✏️"
+                        }
+                        button {
+                            onclick: move|_| {
+                                let id = col.id;
+                                let mut cols = collections.cloned();
+                                cols.retain(|kol|kol.0.id != col.id);
+                                collections.clone().set(cols);
+                                spawn(async move{
+                                    APP.read().delete_collection(id).await;
+                                });
+                            },
+                            "❌"
+                        }
+                        button {
+                            class: "inline-flex items-center text-white bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mb-2",
+                            onclick: move |_| {
+                                let filter = filter.clone();
+                                spawn(async move {
+                                    let col = APP.read().load_collection(col.id).await;
+                                    let cards = col.expand(APP.read().inner().card_provider.clone()).await;
+                                    let session = ReviewState::new_with_filter(cards, filter).await;
+                                    OVERLAY.cloned().set(Box::new(session));
+                                });
+                            },
+                            "{col.name}"
+                        }
                     }
-                    button {
-                        onclick: move |_|{
-                            spawn(async move {
-                                let viewer = ColViewer::new(col.id).await;
-                                OVERLAY.read().set(Box::new(viewer));
-                            });
-                        },
-                        "✏️"
-                    }
-                    button {
-                        onclick: move|_| {
-                            let id = col.id;
-                            let mut cols = collections.cloned();
-                            cols.retain(|kol|kol.id != col.id);
-                            collections.clone().set(cols);
-                            spawn(async move{
-                                APP.read().delete_collection(id).await;
-                            });
-                        },
-                        "❌"
-                    }
+
+                    RecallBar { dist  }
                 }
             }
 
