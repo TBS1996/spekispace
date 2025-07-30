7b0c9341b83d6e8be50f413c6f1927f967deb180
Tor <torberge@outlook.com>
2025-07-30 11:54:14 +0200

    name on review

--- speki/src/overlays/reviewsession.rs
+++ speki/src/overlays/reviewsession.rs
@@ -134,7 +134,7 @@ pub fn ReviewRender(
 
     let history = card.history().to_owned();
 
-    let front = card.front_side().to_owned();
+    let front = card.name().to_owned();
     let back = card.backside().to_owned();
 
     let card2 = card.clone();
