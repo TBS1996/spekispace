ab156e6550a946c9186057b0ed45404a882dd5f6
Tor <torberge@outlook.com>
2025-12-31 09:56:33 +0100

    ref: extract local to module

--- ledgerstore/src/lib.rs
+++ ledgerstore/src/lib.rs
@@ -21,6 +21,7 @@ use uuid::Uuid;
 mod blockchain;
 pub mod ledger_cache;
 mod ledger_item;
+mod local;
 mod read_ledger;
 mod remote;
 use blockchain::{BlockChain, SetUpstream};
@@ -336,71 +337,7 @@ impl<T: LedgerItem> CardChange<T> {
     }
 }
 
-#[derive(Clone)]
-struct Local<T: LedgerItem> {
-    pub inner: FsReadLedger<T>,
-}
-
-impl<T: LedgerItem> Local<T> {
-    pub fn item_path(&self, key: T::Key) -> PathBuf {
-        self.inner.item_path(key)
-    }
-
-    pub fn item_path_create(&self, key: T::Key) -> PathBuf {
-        self.inner.item_path_create(key)
-    }
-}
-
-impl<T: LedgerItem> ReadLedger for Local<T> {
-    type Item = T;
-
-    fn load(&self, key: <Self::Item as LedgerItem>::Key) -> Option<Self::Item> {
-        self.inner.load(key)
-    }
-
-    fn load_ids(&self) -> HashSet<<Self::Item as LedgerItem>::Key> {
-        self.inner.load_ids()
-    }
-
-    fn get_property_cache(
-        &self,
-        cache: PropertyCache<Self::Item>,
-    ) -> HashSet<<Self::Item as LedgerItem>::Key> {
-        self.inner.get_property_cache(cache)
-    }
-
-    fn has_property(
-        &self,
-        key: <Self::Item as LedgerItem>::Key,
-        property: PropertyCache<Self::Item>,
-    ) -> bool {
-        self.inner.has_property(key, property)
-    }
-
-    fn get_reference_cache(
-        &self,
-        key: <Self::Item as LedgerItem>::Key,
-        ty: Option<<Self::Item as LedgerItem>::RefType>,
-        reversed: bool,
-        recursive: bool,
-    ) -> HashSet<<Self::Item as LedgerItem>::Key> {
-        self.inner.get_reference_cache(key, ty, reversed, recursive)
-    }
-
-    fn get_reference_cache_with_ty(
-        &self,
-        key: <Self::Item as LedgerItem>::Key,
-        ty: Option<<Self::Item as LedgerItem>::RefType>,
-        reversed: bool,
-        recursive: bool,
-    ) -> HashSet<(
-        <Self::Item as LedgerItem>::RefType,
-        <Self::Item as LedgerItem>::Key,
-    )> {
-        self.inner
-            .get_reference_cache_with_ty(key, ty, reversed, recursive)
-    }
-}
+use crate::local::Local;
 
 #[derive(Clone, Debug, Hash)]
 pub struct Node<T: LedgerItem> {
